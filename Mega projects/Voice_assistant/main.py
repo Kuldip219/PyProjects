d2149786fbeb854c4e188abe1b663565da81ae4d
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-03-07 09:07:05 +0600

    Updated main.py

--- Mega projects/Voice_assistant/main.py
+++ Mega projects/Voice_assistant/main.py
@@ -9,4 +9,16 @@ if __name__ == "__main__":
         engine.say(audio)
         engine.runAndWait()
 
-    
+    r = sr.Recognizer()
+
+    speak("Hello, I am Gipsy")
+
+    while True:
+        with sr.Microphone() as source:
+            print("Listening...")
+            r.adjust_for_ambient_noise(source, duration=0.5)
+
+            audio = r.listen(source, timeout=5, phrase_time_limit=5)
+
+        print("Recognizing...")
+        
