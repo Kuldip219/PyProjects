acd9f39c63a22b30ff4d66654bd017521e476878
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-03-07 09:03:36 +0600

    Updated main.py
    Added a code that runs pyttsx.

--- Mega projects/Voice_assistant/main.py
+++ Mega projects/Voice_assistant/main.py
@@ -1,4 +1,12 @@
 import speech_recognition as sr
 import pyttsx3
 
-engine = pyttsx3.init()
+if __name__ == "__main__":
+    engine = pyttsx3.init('sapi5')
+    engine.setProperty('rate', 150)
+
+    def speak(audio):
+        engine.say(audio)
+        engine.runAndWait()
+
+    
