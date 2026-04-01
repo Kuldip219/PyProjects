5621bc339c886596dfde3d9d773fb70afa383aea
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-04-01 20:27:19 +0600

    Update main.py
    Added some lines of code but still working on it.

--- Game projects/Shooting game/main.py
+++ Game projects/Shooting game/main.py
@@ -18,10 +18,16 @@ player_speed = 5
 running = True
 while running:
     for event in pygame.event.get():
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player_x -= player_speed
+        if keys[pygame.K_RIGHT]:
+            player_x += player_speed
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill((0, 0, 0))  # Black background
+    pygame.draw.rect(screen, (0, 255, 0), (player_x, player_y, player_width, player_height))
     pygame.display.update()
 
 pygame.quit()
