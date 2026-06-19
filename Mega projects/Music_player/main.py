de646ba4726ac1245f2d9c363460ca7d630016f4
Kuldip Sarker <kuldipsarker219@gmail.com>
2026-06-19 09:39:07 +0600

    Load MP3s from songs folder
    Populate MusicPlayer.songs by scanning the local songs/ directory for .mp3 files. Adds os import and initializes a songs ListProperty in MusicPlayer.__init__ to list available tracks. Also adds three MP3 files to Mega projects/Music_player/songs/ so they appear in the player.

--- Mega projects/Music_player/main.py
+++ Mega projects/Music_player/main.py
@@ -1,11 +1,28 @@
+import os
+
+
+from kivy.app import App
 from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
+
 Builder.load_file('music_player.kv')
 
+
 class MusicPlayer(BoxLayout):
-    pass
+    songs = ListProperty([])
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        songs_folder = "songs"
+
+        if os.path.exists(songs_folder):
+            self.songs = [
+                file for file in os.listdir(songs_folder) 
+                if file.endswith(".mp3")
+            ]
 
 
 class MusicApp(App):
